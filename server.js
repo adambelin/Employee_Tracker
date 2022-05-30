026f95c7a802282650563a052f804b33a34c0a92
AdamBelin <adambelin1992@gmail.com>
2022-05-29 22:03:02 -0400

    added server connections

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const db = require('./db/connection');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extend: false }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+db.connect(err => {
+    if(err) throw err;
+    console.log('Database connected');
+    app.listen(PORT, () => {
+        console.log(`Server running on ${port}`);
+    });
+});
